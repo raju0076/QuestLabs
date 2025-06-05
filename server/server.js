85642475c8cf840f74b4f09b071b8fe956ab6601
raju <gnraju0076@gmail.com>
2025-06-05 23:13:19 +0530

    databaseupdate

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use("/users", formRouter);
 
 connecteDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });
